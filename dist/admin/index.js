5995576adf490e4317f21871ab4435079ae86e9f
Mitchell Kimbrough <mitchell@solspace.com>
2025-10-08 19:57:10 -0700

    Remove dist temporarily

--- dist/admin/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const index = require("../_chunks/index-x3xDWlY9.js");
-module.exports = index.index;
-//# sourceMappingURL=index.js.map
--- dist/admin/index.mjs
+++
@@ -1,5 +0,0 @@
-import { i } from "../_chunks/index-B_EvUV9H.mjs";
-export {
-  i as default
-};
-//# sourceMappingURL=index.mjs.map
